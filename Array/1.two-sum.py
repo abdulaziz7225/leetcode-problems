14eb37329a88fca76e18d5c8ed1943d3995bf292
Abdulaziz Musa <swiftv99@gmail.com>
2024-12-12 17:27:34 +0100

    Create a template to keep all solutions in single format

--- Array/1.two-sum.py
+++ Array/1.two-sum.py
@@ -1,7 +1,13 @@
-# Problem Number: 1. Two Sum
-# Difficulty Level: Easy
-
 """
+Problem Number: 1. Two Sum
+Difficulty Level: Easy
+https://leetcode.com/problems/two-sum/
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+********************************************************************************
+
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
