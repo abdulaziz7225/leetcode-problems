05baf8d47c5694d3feb198706317a353345dbf4e
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-03-13 21:58:10 +0100

    Solve 3355. Zero Array Transformation I

---
+++ Prefix_Sum/Medium/3355.zero-array-transformation-i.py
@@ -0,0 +1,61 @@
+"""
+Problem Number: 3355. Zero Array Transformation I
+Difficulty Level: Medium
+https://leetcode.com/problems/zero-array-transformation-i
+
+********************************************************************************
+
+You are given an integer array nums of length n and a 2D array queries, where queries[i] = [li, ri].
+For each queries[i]:
+Select a subset of indices within the range [li, ri] in nums.
+Decrement the values at the selected indices by 1.
+A Zero Array is an array where all elements are equal to 0.
+Return true if it is possible to transform nums into a Zero Array after processing all the queries sequentially, otherwise return false.
+  
+Example 1:
+Input: nums = [1,0,1], queries = [[0,2]]
+Output: true
+Explanation:
+For i = 0:
+Select the subset of indices as [0, 2] and decrement the values at these indices by 1.
+The array will become [0, 0, 0], which is a Zero Array.
+
+Example 2:
+Input: nums = [4,3,2,1], queries = [[1,3],[0,2]]
+Output: false
+Explanation:
+For i = 0:
+Select the subset of indices as [1, 2, 3] and decrement the values at these indices by 1.
+The array will become [4, 2, 1, 0].
+For i = 1:
+Select the subset of indices as [0, 1, 2] and decrement the values at these indices by 1.
+The array will become [3, 1, 0, 0], which is not a Zero Array.
+  
+Constraints:
+1 <= nums.length <= 10^5
+0 <= nums[i] <= 10^5
+1 <= queries.length <= 10^5
+queries[i].length == 2
+0 <= li <= ri < nums.length
+"""
+
+from typing import List
+
+
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        diff_array = [0] * (n + 1)
+        for left, right in queries:
+            diff_array[left] -= 1
+            diff_array[right + 1] += 1
+
+        curr_sum = 0
+        for i in range(n):
+            curr_sum += diff_array[i]
+            if curr_sum + nums[i] > 0:
+                return False
+        return True
+
+# Time Complexity: O(2 * n) ==> O(n)
+# Space Complexity: O(n)
