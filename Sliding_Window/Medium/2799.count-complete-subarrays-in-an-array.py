178648c6dbc8966b064a98ba485a50df19217c28
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-04-25 01:20:44 +0200

    Solve 2799. Count Complete Subarrays in an Array

---
+++ Sliding_Window/Medium/2799.count-complete-subarrays-in-an-array.py
@@ -0,0 +1,55 @@
+"""
+Problem Number: 2799. Count Complete Subarrays in an Array
+Difficulty Level: Medium
+https://leetcode.com/problems/count-complete-subarrays-in-an-array
+
+********************************************************************************
+
+You are given an array nums consisting of positive integers.
+We call a subarray of an array complete if the following condition is satisfied:
+The number of distinct elements in the subarray is equal to the number of distinct elements in the whole array.
+Return the number of complete subarrays.
+A subarray is a contiguous non-empty part of an array.
+  
+Example 1:
+Input: nums = [1,3,1,2,2]
+Output: 4
+Explanation: The complete subarrays are the following: [1,3,1,2], [1,3,1,2,2], [3,1,2] and [3,1,2,2].
+
+Example 2:
+Input: nums = [5,5,5,5]
+Output: 10
+Explanation: The array consists only of the integer 5, so any subarray is complete. The number of subarrays that we can choose is 10.
+  
+Constraints:
+1 <= nums.length <= 1000
+1 <= nums[i] <= 2000
+"""
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        freq = defaultdict(int)
+        total_unique = len(set(nums))
+        result = 0
+
+        left = 0
+        for right in range(len(nums)):
+            freq[nums[right]] += 1
+
+            while len(freq) == total_unique:
+                key = nums[left]
+                freq[key] -= 1
+                if freq[key] == 0:
+                    del freq[key]
+                left += 1
+
+            result += left
+
+        return result
+
+# Time Complexity: O(n + 2 * n) ==> O(n)
+# Space Complexity: O(n)
