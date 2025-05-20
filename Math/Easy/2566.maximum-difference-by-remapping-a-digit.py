ac85605570fb2873382ff882993ba568a21df32a
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-05-20 22:05:22 +0200

    Solve 2566. Maximum Difference by Remapping a Digit

---
+++ Math/Easy/2566.maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,61 @@
+"""
+Problem Number: 2566. Maximum Difference by Remapping a Digit
+Difficulty Level: Easy
+https://leetcode.com/problems/maximum-difference-by-remapping-a-digit
+
+********************************************************************************
+
+You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another digit.
+Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+Notes:
+When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+Bob can remap a digit to itself, in which case num does not change.
+Bob can remap different digits for obtaining minimum and maximum values respectively.
+The resulting number after remapping can contain leading zeroes.
+  
+Example 1:
+Input: num = 11891
+Output: 99009
+Explanation: 
+To achieve the maximum value, Bob can remap the digit 1 to the digit 9 to yield 99899.
+To achieve the minimum value, Bob can remap the digit 1 to the digit 0, yielding 890.
+The difference between these two numbers is 99009.
+
+Example 2:
+Input: num = 90
+Output: 99
+Explanation:
+The maximum value that can be returned by the function is 99 (if 0 is replaced by 9) and the minimum value that can be returned by the function is 0 (if 9 is replaced by 0).
+Thus, we return 99.
+  
+Constraints:
+1 <= num <= 10^8
+"""
+
+
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        digit_list = list(map(int, str(num)))
+
+        max_val, min_val = 0, 0
+        first_non_nine = None
+        first_digit = digit_list[0]
+
+        for curr_digit in digit_list:
+            if first_non_nine is None and curr_digit != 9:
+                first_non_nine = curr_digit
+
+            if curr_digit == first_non_nine:
+                max_val = 10 * max_val + 9
+            else:
+                max_val = 10 * max_val + curr_digit
+
+            if curr_digit == first_digit:
+                min_val = 10 * min_val
+            else:
+                min_val = 10 * min_val + curr_digit
+
+        return max_val - min_val
+
+# Time Complexity: O(log10(num)) ==> O(log(num))
+# Space Complexity: O(log10(num)) ==> O(log(num))
