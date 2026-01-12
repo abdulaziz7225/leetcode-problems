5ed1b20c4961faa6d9e26ed98b1e9857dfe37d17
Abdulaziz Musaev <amusaev7225@gmail.com>
2026-01-12 15:40:22 +0100

    Solve 543. Diameter of Binary Tree

---
+++ depth-first-search/easy/543.diameter_of_binary_tree.py
@@ -0,0 +1,54 @@
+"""
+Problem Number: 543. Diameter of Binary Tree
+Difficulty Level: Easy
+Link: https://leetcode.com/problems/diameter-of-binary-tree/
+
+********************************************************************************
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them.
+
+Example 1:
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+Input: root = [1,2]
+Output: 1
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+"""
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.result = 0
+        self.dfs(root)
+        return self.result
+
+    def dfs(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left_sum = self.dfs(root.left)
+        right_sum = self.dfs(root.right)
+
+        self.result = max(self.result, left_sum + right_sum)
+        return 1 + max(left_sum, right_sum)
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
